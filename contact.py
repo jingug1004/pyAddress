488b7a3430858ba7bb11936cdcfbb121d8af9d3f
jingug1004 <jingug1004@gmail.com>
2019-03-27 20:01:52 +0900

    190327 Algorithm Trading by Learned Python

--- contact.py
+++ contact.py
@@ -26,7 +26,10 @@ def set_contact():
     phone_number = input("Phone Number: ")
     e_mail = input("E-mail: ")
     addr = input("Address: ")
-    print(name, phone_number, e_mail, addr)
+    contact = Contact(name, phone_number, e_mail, addr)
+    # print(name, phone_number, e_mail, addr)
+    return contact
+
 
 def print_menu():
     print("1. 연락처 입력")
@@ -36,10 +39,64 @@ def print_menu():
     menu = input("메뉴선택: ")
     return int(menu)
 
+
+def print_contact(contact_list):
+    for contact in contact_list:
+        contact.print_info()
+
+
+def delete_contact(contact_list, name):
+    for i, contact in enumerate(contact_list):
+        if contact.name == name:
+            del contact_list[i]
+
+
+def store_contact(contact_list):
+    f = open("contact_db.txt", "wt")
+    for contact in contact_list:
+        f.write(contact.name + '\n')
+        f.write(contact.phone_number + '\n')
+        f.write(contact.e_mail + '\n')
+        f.write(contact.addr + '\n')
+    f.close()
+
+
+def load_contact(contact_list):
+    f = open("contact_db.txt", "rt")
+    lines = f.readlines()
+    num = len(lines) / 4
+    num = int(num)
+
+    for i in range(num):
+        name = lines[4 * i].rstrip('\n')
+        phone = lines[4 * i + 1].rstrip('\n')
+        email = lines[4 * i + 2].rstrip('\n')
+        addr = lines[4 * i + 3].rstrip('\n')
+        contact = Contact(name, phone, email, addr)
+        contact_list.append(contact)
+    f.close()
+
+
 def run():
     # kim = Contact('김일구', '010-8812-1193', 'ilgu.kim@python.com', 'Seoul')
     # kim.print_info()
-    set_contact()
+    contact_list = []
+    load_contact(contact_list)
+    while 1:
+        menu = print_menu()
+        if menu == 1:
+            contact = set_contact()
+            contact_list.append(contact)
+        elif menu == 2:
+            print_contact(contact_list)
+        elif menu == 3:
+            name = input("Name: ")
+            delete_contact(contact_list, name)
+        elif menu == 4:
+            store_contact(contact_list)
+            break
+
+    # set_contact()
 
 
 if __name__ == "__main__":
